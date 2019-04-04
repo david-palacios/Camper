a4daa845222d7020eb16b9097878febbbe0b6412
JohnYB1963 <jyenterbriars@gmail.com>
2019-04-04 16:50:36 -0500

    Added gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -2,25 +2,29 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/home')
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html', data = "data here")
 
-@app.route('/getData', methods=['GET'])
-def getData():
+
+@app.route('/getdata', methods=['GET'])
+def getdata():
     if request.method == 'GET':
         return render_template('getData.html', data=request.args.get('data'))
     else:
         return "404"
 
-@app.route('/getData/<num>', methods=['GET'])
-def getData2(num):
+
+@app.route('/getdata/<num>', methods=['GET'])
+def getdata2(num):
     if request.method == 'GET':
         return render_template('getData.html', data=num)
     else:
         return "404"
 
-@app.route('/postForm')
-def postForm():
+
+@app.route('/postform')
+def postform():
     return render_template('postForm.html')

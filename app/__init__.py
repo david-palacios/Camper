1f16d23f123413cc2893f70a846a0d4c8408efb9
JohnYB1963 <jyenterbriars@gmail.com>
2019-04-18 14:10:53 -0500

    Add encryption for api key

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template, request
-# from simplecrypt import encrypt, decrypt
+import os
+
+from Crypto.Cipher import XOR
+import base64
+
+def encrypt(key, plaintext):
+  cipher = XOR.new(key)
+  return base64.b64encode(cipher.encrypt(plaintext))
+
+def decrypt(key, ciphertext):
+  cipher = XOR.new(key)
+  return cipher.decrypt(base64.b64decode(ciphertext))
 
 app = Flask(__name__)
 
@@ -11,7 +22,7 @@ def home():
 
 @app.route('/home/map')
 def getmap():
-    apikey = "AIzaSyB31C7CeNH_CyGtLBaKz7SlWLmgkLvB7kE"
+    apikey = open("./res/key.txt")
     return render_template('testGoogleAPI.html', key=apikey)
 
 
@@ -37,5 +48,14 @@ def postform():
 
 @app.route('/find')
 def find():
-    return render_template('find.html', key="AIzaSyB31C7CeNH_CyGtLBaKz7SlWLmgkLvB7kE")
+    curr_dir = os.path.dirname(os.path.realpath(__file__))
+    key = open(curr_dir + "/res/key.txt").read()
 
+    # encripted = encrypt(open(curr_dir+"/res/key.txt").read(), "AIzaSyB31C7CeNH_CyGtLBaKz7SlWLmgkLvB7kE")
+    # write = open(curr_dir+"/res/encrypted.txt", "wb")
+    # write.write(encripted)
+    # write.close()
+    
+    apikey = decrypt(key, open(curr_dir + "/res/encrypted.txt", "rb").read()).decode("utf-8")
+
+    return render_template('find.html', key=apikey)
